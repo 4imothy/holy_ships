b01568770c234b22cd60300ead73e8e57a57de06
Ben Luo <sbluo2002@gmail.com>
2024-10-19 02:05:16 -0400

    Added Folders and Start Menu UI
    Remember to add any art you find to the doc

---
+++ scripts/main_menu.gd
@@ -0,0 +1,26 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_start_button_pressed() -> void:
+	print("Start Button Pressed")
+	get_tree().change_scene_to_file("res://scenes/level_00/game.tscn")
+
+
+func _on_options_button_pressed() -> void:
+	print("Options Button Pressed")
+	get_tree().change_scene_to_file("res://scenes/main_menu/options_menu.tscn")
+
+
+func _on_exit_button_pressed() -> void:
+	print("Exit Button Pressed")
+	get_tree().quit()
