dbb9ea9d0247938b925e83df63917f3c153af93a
Ben Luo <sbluo2002@gmail.com>
2024-10-20 16:45:04 -0400

    building multiplayer logic into lobby
    Lobby is main scene where game scene is loaded into

--- scripts/lobby_menu.gd
+++ scripts/lobby_menu.gd
@@ -1,31 +1,50 @@
-extends Control
+extends Node
 
+@export var level_container: Node
+@export var level_scene: PackedScene
 @export var ip_line_edit: LineEdit
 @export var status_label: Label
+@export var not_connected_hbox: HBoxContainer
+@export var host_hbox: HBoxContainer
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	multiplayer.connection_failed.connect(_on_connection_failed)
+	multiplayer.connection_failed.connect(_on_connected_to_server)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
+### Button Presses ###
 func _on_host_button_pressed() -> void:
-	Lobby.create_game()
+	not_connected_hbox.hide()
+	if not Lobby.create_game():
+		not_connected_hbox.show()
+		status_label.text = "Connection Status: Failed to Host"
+	else:
+		host_hbox.show()
+		status_label.text = "Connection Status: Hosting!"
 
 
 func _on_join_button_pressed() -> void:
+	not_connected_hbox.hide()
 	status_label.text = "Connection Status: Connecting..."
 	if ip_line_edit:
 		Lobby.join_game(ip_line_edit.text)
 
 
 func _on_start_button_pressed() -> void:
-	pass # Replace with function body.
+	change_level.call_deferred(level_scene)
+
+func change_level(scene):
+	level_container.add_child(scene.instantiate())
 
 func _on_exit_lobby_button_pressed() -> void:
 	print("Exit Lobby Menu Button Pressed")
 	get_tree().change_scene_to_file("res://scenes/main_menu/main_menu.tscn")
+	
+### Helper Functions ###
+func _on_connection_failed():
+	status_label.text = "Connection Status: Failed to Connect"
+	not_connected_hbox.show()
+	
+func _on_connected_to_server():
+	status_label.text = "Connection Status: Connected!"
