93f6b2731077c4d7a8d5d94a63c34a2dd51af652
Timothy Cronin <tcroniniv@gmail.com>
2024-10-25 17:43:17 -0400

    add pressure plate functionality

---
+++ GameObjects/door.gd
@@ -0,0 +1,61 @@
+# TODO make the door work both ways
+extends Area2D
+
+@export var rdoor_label: Label
+@export var rdoor: Sprite2D
+@export var ldoor: Sprite2D
+@export var spawn_point: Node2D
+
+var open_door = false
+var rdoor_open = false
+var ldoor_open = false
+var door_open = false
+const door_speed: float = 100.0
+var target_offset: float = 20 
+
+var rdoor_original_position: Vector2
+var ldoor_original_position: Vector2
+
+func _ready():
+	# Store the original positions of the doors
+	rdoor_original_position = rdoor.position
+	ldoor_original_position = ldoor.position
+
+func set_number(number: String):
+	rdoor_label.text = number
+
+func open():
+	open_door = true
+
+func close():
+	open_door = false
+
+func _process(delta: float):
+	# if multiplayer.get_unique_id() != 1:
+		# return
+	if open_door:
+		if rdoor.position.x < rdoor_original_position.x + target_offset:
+			rdoor.position.x += door_speed * delta
+			rdoor_open = false
+		else:
+			rdoor_open = true
+		if ldoor.position.x > ldoor_original_position.x - target_offset:
+			ldoor.position.x -= door_speed * delta
+			ldoor_open = false
+		else:
+			ldoor_open = true
+		if ldoor_open and rdoor_open:
+			door_open = true
+	else:
+		if rdoor.position.x > rdoor_original_position.x:
+			rdoor.position.x -= door_speed * delta
+		if ldoor.position.x < ldoor_original_position.x:
+			ldoor.position.x += door_speed * delta
+		door_open = false
+
+
+func _on_area_entered(area: Area2D) -> void:
+	if !door_open or !area.is_in_group('feet'): 
+		return
+	var player = area.get_parent()
+	player.global_position = spawn_point.global_position
--- GameObjects/pressure_plate.gd
+++ GameObjects/pressure_plate.gd
@@ -1,19 +1,29 @@
+# TODO the area entered and probably a bunch of other code is happening
+# on all machines when it can really just happen on host and get shared
+# TODO fix the player count to be shared or only run on Host? If 
+# doors are closing when one player gets off out of two
 extends Area2D
 
 const PIXEL_DIM = 64
 var player_count = 0
-@onready var sprite = $Sprite2D
+@export var sprite: Sprite2D
+@export var pp_label: Label
+@export var door: Node2D
 
-func shift_sprite_region(offset_x: int) -> void:
-	var region = sprite.region_rect
-	region.position.x += offset_x
-	sprite.region_rect = region
+func _ready() -> void:
+	pp_label.text = name.right(1)
+	door.set_number(name.right(1))
+
+func shift_sprite_region(direction: int) -> void:
+	pp_label.position.y += direction * 4
+	sprite.region_rect.position.x += direction * PIXEL_DIM
 
 func _on_area_entered(area: Area2D) -> void:
 	if !area.is_in_group('feet'):
 		return
 	if player_count == 0:
-		shift_sprite_region(PIXEL_DIM)
+		door.open()
+		shift_sprite_region(1)
 	player_count += 1
 
 func _on_area_exited(area: Area2D) -> void:
@@ -21,4 +31,5 @@ func _on_area_exited(area: Area2D) -> void:
 		return
 	player_count -= 1
 	if player_count == 0:
-		shift_sprite_region(-PIXEL_DIM)
+		door.close()
+		shift_sprite_region(-1)
