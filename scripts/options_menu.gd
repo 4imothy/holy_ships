9c597c2a4fc9aaf55ba7b792b986988544769206
Anthony Wang <anthonywang64@gmail.com>
2024-12-03 23:22:04 -0500

    switched audio settings to use AudioServer

--- scripts/options_menu.gd
+++ scripts/options_menu.gd
@@ -8,7 +8,7 @@ func _ready() -> void:
 	if main_menu_music:
 		$VBoxContainer/Volume.min_value = -80  
 		$VBoxContainer/Volume.max_value = 0
-		$VBoxContainer/Volume.value = main_menu_music.volume_db
+		$VBoxContainer/Volume.value = AudioServer.get_bus_volume_db(0)
 		
 func _on_exit_options_button_pressed() -> void:
 	queue_free()
@@ -19,13 +19,18 @@ func _on_texture_rect_mouse_entered() -> void:
 # Called when the slider value changes
 func _on_volume_value_changed(value: float) -> void:
 	if main_menu_music:
-		main_menu_music.volume_db = value / 5
+		if value < 0:
+			AudioServer.set_bus_volume_db(0, value / 2)  # Set volume for bus 0
+		if value == -80:
+			AudioServer.set_bus_volume_db(0, -80)  # Mute the audio on bus 0
+			
+			
 
 
 # Called when the mute checkbox is toggled
 func _on_mute_toggled(toggled_on: bool) -> void:
 	if main_menu_music:
 		if toggled_on:
-			main_menu_music.volume_db = -80  # Mute the audio
+			AudioServer.set_bus_volume_db(0, -80)  # Mute audio on bus 0
 		else:
-			main_menu_music.volume_db = $VBoxContainer/Volume.value 
+			AudioServer.set_bus_volume_db(0, $VBoxContainer/Volume.value / 2)  # Set volume for bus 0 
